5457c4874a7570c3613ec496ea3b3a4fa628b17a
David Etiosa Ojo <davidetiosaojo@gmail.com>
2021-08-28 05:20:04 +0100

    28-08-2021
    Bug fixes
    - line-through not showing on iOS
    - Spacing between input field and button
    - Bg color of task item
    - Page scrolling when modal is open
    - Progress bar border radius
    
    Feats added
    - Set up local storage object
    - dynamically add task to local storage object
    - Mark as completed [class addition. Updating local storage object coming soon]
    - Show/hide completed task items section
    - Truncate group title text
    - Some minor restructuring

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,23 @@ const addGroupButton = document.getElementById("addGroupModal--button")
 const defaultAddTaskField = document.getElementById("defaultAddTaskModal--field")
 const defaultAddTaskButton = document.getElementById("defaultAddTaskModal--button")
 
+const defaultUncompletedTasksBox = document.getElementById("default-uncompleted-tasks--box")
+
+let todoAppData = {
+    defaultuncompletedtasks: []
+}
+const getStoredTodoAppData = localStorage.getItem("todoAppData")
+const parsedTodoAppData = JSON.parse(getStoredTodoAppData)
+
+// displays items from local storage
+if(parsedTodoAppData) {
+    todoAppData = parsedTodoAppData
+
+    generateDefaultTask(todoAppData.defaultuncompletedtasks)
+}
+
+const defTaskItem = document.querySelectorAll(".section--box-item")
+
 // creates group
 function createGroup() {
     if(addGroupField.value) {
@@ -18,10 +35,33 @@ addGroupButton.addEventListener("click", ()=> {
     createGroup()
 })
 
-// creates default task
-function createDefaultTask() {
+// generate random string of characters for task id
+function generateTaskId(length) {
+    let taskId = ""
+    let idString = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+    for ( let i = 0; i < length; i++ ) {
+        taskId += idString.charAt(Math.floor(Math.random() * idString.length))
+   }
+   return taskId
+}
+
+// set item to local storage
+function setItemToLS() {
+    localStorage.setItem("todoAppData", JSON.stringify(todoAppData))
+}
+
+// adds task to default uncompleted tasks array
+function addDefaultTask() {
     if(defaultAddTaskField.value) {
-        console.log(defaultAddTaskField.value)
+        todoAppData.defaultuncompletedtasks.push({
+            name: defaultAddTaskField.value,
+            completed: false,
+            id: `${generateTaskId(8)}${Math.floor(Math.random() * 1000)}-${Math.floor(Math.random() * 200)}${todoAppData.defaultuncompletedtasks.length}-${generateTaskId(16)}${Math.floor(Math.random() * 1000)}`
+        })
+
+        // adds item to local storage
+        setItemToLS()
+        generateDefaultTask(todoAppData.defaultuncompletedtasks)
     }
 
     defaultAddTaskField.value = ""
@@ -29,12 +69,23 @@ function createDefaultTask() {
 }
 
 defaultAddTaskButton.addEventListener("click", ()=> {
-    createDefaultTask()
+    addDefaultTask()
 })
 
-// document.addEventListener("keyup", (e)=> {
-//     if(e.key == "Enter") {
-//         createGroup()
-//     }
-// })
+// sets task item state
+defTaskItem.forEach(item => item.addEventListener("click", (e)=> {
 
+    if(!(item.classList.contains("completed"))) {
+        item.classList.add("completed")
+    } else if(item.classList.contains("completed")) {
+        item.classList.remove("completed")
+    }
+    
+}))
+
+// TODO: work on completed task empty state
+
+// TODO: work on task groups
+document.querySelectorAll(".group--item").forEach(grpItm => grpItm.addEventListener("click", ()=> {
+    console.log(generateTaskId(32))
+}))
--- scripts/ui.js
+++ scripts/ui.js
@@ -37,13 +37,24 @@ const overlayIE = document.querySelectorAll(".overlay--inputModal")
 
 const textArea = document.querySelectorAll("textarea")
 
+const sectionMain = document.querySelectorAll(".section")
+const sectionBox = document.querySelectorAll(".section--box")
+const titleDropIcon = document.querySelectorAll(".section--head_dropIcon")
+
 // shows current day, date and month
 mainDateEl.textContent= `${currentDay}, ${currentDate} ${currentMonth}`
 
+// stops scrolling on body
+stopBodyScroll = () => {
+    document.body.style.overflow = 'hidden'
+}
+
 // opens add group modal
-function addGM() {
+addGM = () => {
     overlay.classList.add("active")
     overlay.classList.add("grpMod")
+
+    stopBodyScroll()
 }
 
 addGroupBtn.addEventListener("click", ()=> {
@@ -52,23 +63,31 @@ addGroupBtn.addEventListener("click", ()=> {
 
 
 // opens main add task modal
-function mainATM() {
+mainATM = () => {
     overlay.classList.add("active")
     overlay.classList.add("tskMod")
+
+    stopBodyScroll()
 }
 
 mainAddTaskBtn.addEventListener("click", ()=> {
     mainATM()
 })
 
-// removes overlay and modals
-function removeOMs() {
+// removes overlay, modals and their input element values. makes body scrollable
+removeOMs = () => {
     overlay.classList.remove("active")
     overlay.classList.remove("grpMod")
     overlay.classList.remove("tskMod")
+
+    document.body.style.overflow = 'visible'
 }
 
 overlay.addEventListener("click", ()=> {
+    document.getElementById("defaultAddTaskModal--field").value = ""
+    document.getElementById("addGroupModal--field").value = ""
+    gccUpdate(document.getElementById("addGroupModal--field").value)
+
     removeOMs()
 })
 
@@ -88,8 +107,76 @@ textArea.forEach(Area => Area.addEventListener("keyup", (e)=> {
 }))
 
 // textarea character count
-function gccUpdate(str) {
+gccUpdate = (str) => {
     const maxLength = document.getElementById("addGroupModal--field").getAttribute("maxlength")
     const talengthCount = maxLength - str.length
     document.querySelector(".charCount").innerHTML = `${talengthCount} characters left`
 }
+
+// generates task item from tasks array
+generateDefaultTask = (defTask) => {
+    let defaultTaskItems = ""
+    for(let i = 0; i < defTask.length; i++) {
+        defaultTaskItems += `
+            <li class="section--box-item" id="task-${defTask[i].id}">
+                <div class="check-icon-box">
+                    <div class="check-icon-box--inner"></div>
+                </div>
+                <p class="section--box-item_value">${defTask[i].name}</p>
+            </li>
+        `
+    }
+    defaultUncompletedTasksBox.innerHTML = defaultTaskItems
+}
+
+// default completed tasks dropdown trigger
+titleDropIcon.forEach(dropIcon => dropIcon.addEventListener("click", ()=> {
+    let dIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="eva eva-chevron-down" fill="#3446ee"><g data-name="Layer 2"><g data-name="chevron-down"><rect width="24" height="24" opacity="0"></rect><path d="M12 15.5a1 1 0 0 1-.71-.29l-4-4a1 1 0 1 1 1.42-1.42L12 13.1l3.3-3.18a1 1 0 1 1 1.38 1.44l-4 3.86a1 1 0 0 1-.68.28z"></path></g></g></svg>`
+    let uIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="eva eva-chevron-up" fill="#3446ee"><g data-name="Layer 2"><g data-name="chevron-up"><rect width="24" height="24" transform="rotate(180 12 12)" opacity="0"></rect><path d="M16 14.5a1 1 0 0 1-.71-.29L12 10.9l-3.3 3.18a1 1 0 0 1-1.41 0 1 1 0 0 1 0-1.42l4-3.86a1 1 0 0 1 1.4 0l4 4a1 1 0 0 1 0 1.42 1 1 0 0 1-.69.28z"></path></g></g></svg>`
+
+    if(dropIcon.firstElementChild.classList.contains("eva-chevron-up")) {
+        dropIcon.innerHTML = dIcon
+        dropDownOpen()
+
+    } else if(dropIcon.firstElementChild.classList.contains("eva-chevron-down")) {
+        dropIcon.innerHTML = uIcon
+        dropDownClosed()
+    }
+}))
+
+// shows defualt completed tasks
+dropDownOpen = () => {
+    // for(let i = 0; i < sectionBox.length; i++) {
+        sectionBox[1].classList.add("drop-closed")
+        sectionBox[1].classList.remove("drop-opened")
+        setTimeout( () => {
+            sectionBox[1].classList.add("d-none")
+        }, 200)
+    // }
+}
+
+// hides defualt completed tasks
+dropDownClosed = () => {
+    // for(let i = 0; i < sectionBox.length; i++) {
+        sectionBox[1].classList.remove("drop-closed")
+        sectionBox[1].classList.add("drop-opened")
+        setTimeout( () => {
+            sectionBox[1].classList.remove("d-none")
+        }, 100)
+    // }
+}
+
+// truncate group title
+const gtStr = document.querySelectorAll(".group--item-details_name")
+function truncateGroupTitle(str, num){
+    if(str.length > num) {
+        let subStr = str.substring(0, num)
+        return subStr + "..."
+    } else {
+        return str
+    }
+}
+
+gtStr.forEach(gStr => {
+    gStr.innerText = truncateGroupTitle(gStr.innerText, 38)
+});
